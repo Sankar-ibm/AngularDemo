aeab0a945c761c4ca90774be594b10bee5532561
Sankar-ibm <57795720+Sankar-ibm@users.noreply.github.com>
2021-06-15 19:11:50 +0530

    Update build_and_push_image.sh

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -54,6 +54,7 @@ lookup_image=$( ibmcloud cr images | grep "us.icr.io/$cr_namespace/web-frontend:
 # Push the image                                                           #
 ############################################################################
 echo "Pushing image to registry"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push us.icr.io/$cr_namespace/web-frontend:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH
 docker push us.icr.io/$cr_namespace/web-frontend:latest
 ############################################################################

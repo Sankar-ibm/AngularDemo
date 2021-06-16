7bb84a3964af360f618c7294741ad850569fdfc3
Sankar-ibm <57795720+Sankar-ibm@users.noreply.github.com>
2021-06-16 22:32:18 +0530

    Update openshift_deploy.sh

--- openshift_deploy.sh
+++ openshift_deploy.sh
@@ -2,7 +2,7 @@
 
 curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.1/linux/oc.tar.gz | tar xz
 ./oc login --token=NY5RTLgNq7Pa9ODa-CCP2TGHx2R4fG6sxbOIsIO99o8 --server=https://c100-e.us-east.containers.cloud.ibm.com:31217
-docker login -u iamapikey -p $CLOUD_API_KEY us.icr.io
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 ./oc create secret generic regtoken --from-file .dockerconfigjson=/home/travis/.docker/config.json --type kubernetes.io/dockerconfigjson
-./oc import-image web-frontend-dev --confirm --from us.icr.io/web-frontend-dev/web-frontend:latest
-./oc new-app --as-deployment-config -i web-frontend-dev
+./oc import-image frontend --confirm --from us.icr.io/web-frontend-dev/frontend:latest
+./oc new-app --as-deployment-config -i frontend

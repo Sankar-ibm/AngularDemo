6d9865f9e1b8a97c6d7234f52f9738c84f9e6a19
Sankar-ibm <57795720+Sankar-ibm@users.noreply.github.com>
2021-06-16 22:24:56 +0530

    Update openshift_deploy.sh

--- openshift_deploy.sh
+++ openshift_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.1/linux/oc.tar.gz | tar xz
-./oc login --token=OLHiAQNW30OQCyKGNCmzDoStW1mgSnVZis82X6q1_WU --server=https://c100-e.us-east.containers.cloud.ibm.com:31217
+./oc login --token=NY5RTLgNq7Pa9ODa-CCP2TGHx2R4fG6sxbOIsIO99o8 --server=https://c100-e.us-east.containers.cloud.ibm.com:31217
 docker login -u iamapikey -p $CLOUD_API_KEY us.icr.io
-# ./oc create secret generic regtoken --from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json --type kubernetes.io/dockerconfigjson
-./oc import-image web-frontend --confirm --reference-policy local --from us.icr.io/web-frontend-dev/web-frontend:latest
-./oc new-app --as-deployment-config -i web-frontend
+./oc create secret generic regtoken --from-file .dockerconfigjson=/home/travis/.docker/config.json --type kubernetes.io/dockerconfigjson
+./oc import-image web-frontend-dev --confirm --from us.icr.io/web-frontend-dev/web-frontend:latest
+./oc new-app --as-deployment-config -i web-frontend-dev
